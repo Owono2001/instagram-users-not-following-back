82c2f4348ff95baa3a8709245248ff9ffec45324
J <mosinabrejp@students.nu-dasma.edu.ph>
2024-07-23 12:50:44 +0800

    main js script

---
+++ main.js
@@ -0,0 +1,55 @@
+// Assuming 'followers.json' is accessible at a URL
+const followersJSON = 'followers_1.json';
+const followingsJSON = 'following_1.json';
+const container = document.getElementById("container");
+
+const followingNames = [];
+const followersNames = [];
+
+
+function fetchAndProcessJSON(jsonFile, namesArray) {
+  fetch(jsonFile)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Fetched data:', data); 
+      data.forEach(item => {
+        if (!item.string_list_data) {
+          console.error('string_list_data not found on item:', item);
+          return;
+        }
+        item.string_list_data.forEach(stringData => {
+          console.log('Found name:', stringData.value);
+          namesArray.push(stringData.value);
+        });
+      });
+
+      console.log('Names:', namesArray);
+
+      if (namesArray.length > 0) {
+        getUsersWhoDonotFollowBack();
+      }
+    })
+    .catch(error => {
+      console.error('Failed to fetch data:', error);
+    });
+    
+}
+
+function getUsersWhoDonotFollowBack(){
+    const usersWhoDontFollowBack = followingNames.filter(name => !followersNames.includes(name));
+    if (usersWhoDontFollowBack.length > 0) {
+      const formattedNames = usersWhoDontFollowBack.map(name => `@${name}`).join('<br>');
+      container.innerHTML += `<p>Users who don't follow back:<br>${formattedNames}</p>`;
+      console.log('Users who dont follow back:', usersWhoDontFollowBack);
+    } else {
+      console.log('No users who dont follow back found');
+    }
+}
+
+fetchAndProcessJSON(followersJSON, followersNames);
+fetchAndProcessJSON(followingsJSON, followingNames);
